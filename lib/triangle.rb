be7dc2017cfc332ec25dad298aa28da047f15f01
Michael Harris <xero5k@yahoo.com>
2022-01-08 16:13:14 -0600

    commit tree 8b693ce10014e1b759757df2fb0091ccc8c62cd4

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
+
+    attr_accessor :side_a, :side_b, :side_c
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a    
+    @side_b = side_b    
+    @side_c = side_c    
+  end 
+
+  def kind
+    if    side_a <= 0 || 
+          side_b <= 0 ||
+          side_c <= 0
+      raise TriangleError
+    elsif side_a + side_b <= side_c ||
+          side_a + side_c <= side_b ||
+          side_b + side_c <= side_a 
+      raise TriangleError
+    elsif side_a == side_b && 
+          side_b == side_c 
+      :equilateral
+    elsif side_a == side_b || 
+          side_b == side_c ||
+          side_c == side_a
+      :isosceles
+    elsif side_a != side_b && 
+          side_b != side_c &&
+          side_c != side_a
+      :scalene
+# (equilateral, isosceles, scalene)
+    else 
+    end 
+  end 
+
+  class TriangleError < StandardError
+    def message
+      "One or more equal Zero or are negative"
+    end 
+  end 
+
 end
